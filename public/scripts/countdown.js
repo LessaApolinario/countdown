00085adbd16fb84ec80631586ea570b595e3ffa4
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-07 18:25:45 -0300

    Add timer working

---
+++ public/scripts/countdown.js
@@ -0,0 +1,65 @@
+const timer = document.querySelector('h2.timer');
+const button = document.querySelector('button.subscribe');
+
+// The string that represents the time
+const timerValue = timer.textContent;
+
+// Catching the elements for the countdown
+let timerDays = document.querySelector('span.days');
+let timerHours = document.querySelector('span.hours');
+let timerMinutes = document.querySelector('span.minutes');
+let timerSeconds = document.querySelector('span.seconds');
+
+// Catching the numbers
+let days = Number(timerDays.textContent);
+let hours = Number(timerHours.textContent);
+let minutes = Number(timerMinutes.textContent);
+let seconds = Number(timerSeconds.textContent);
+
+const createCountdownTimer = () => {
+  const countdown = setInterval(() => {
+    seconds--;
+    timerSeconds.textContent = seconds;
+
+    if (seconds === 0) {
+      timerSeconds.textContent = 59;
+      seconds = 59;
+
+      minutes--;
+      timerMinutes.textContent = minutes;
+    }
+
+    if (minutes === 0) {
+      timerMinutes.textContent = 59;
+      minutes = 59;
+
+      hours--;
+      timerHours.textContent = hours;
+    }
+
+    if (hours === 0) {
+      timerHours.textContent = 59;
+      hours = 59;
+
+      days--;
+      timerDays.textContent = days;
+    }
+
+    if (days === 0) {
+      clearInterval(countdown);
+    }
+  }, 1000);
+};
+
+button.addEventListener('click', () => {
+  const hasStartClass = button.classList.contains('start');
+
+  if (!hasStartClass) {
+    button.classList.add('start');
+    createCountdownTimer();
+  } else {
+    button.classList.remove('start');
+    clearInterval(createCountdownTimer());
+  }
+});
+// createCountdownTimer();
